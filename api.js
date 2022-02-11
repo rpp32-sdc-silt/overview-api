9daa8926ea283443daf2413337b4b93af69f3a9c
mendelorian <ipanda303@gmail.com>
2022-02-10 22:40:09 -0800

    Added specific product API, planned out styles API

--- api.js
+++ api.js
@@ -23,4 +23,56 @@ async function getProducts(pageArg, countArg) {
   }
 }
 
+async function getSpecificProduct(id) {
+  var productQuery = () => {
+    var res = pool.query('SELECT * FROM product WHERE id=$1', [id]);
+    return res;
+  };
+
+  var featuresQuery = () => {
+    var res = pool.query('SELECT feature, value from features WHERE product_id=$1', [id]);
+    return res;
+  };
+
+  try {
+    var [ productResult, featuresResult ]= await Promise.all([productQuery(), featuresQuery()]);
+    var result = productResult.rows[0];
+    result.features = featuresResult.rows;
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getStyles(productId) {
+  var styleQuery = () => {
+    var res = pool.query('SELECT * FROM styles WHERE productId=$1', [productId]);
+    return res;
+  }
+
+
+  // Need to get the style ids first, then for each get the photos and skus
+  // await the styleQuery, then loop through results, query for photos and skus in parallel
+  // use Promise.all to aggegrate the data
+
+  var photoQuery = () => {
+    var res = pool.query('SELECT thumbnail_url, url FROM photos WHERE styleId', []);
+    return res;
+  }
+
+  var skusQuery = () => {
+    var res = pool.query('SELECT', []);
+  }
+
+  try {
+    var [ photoResult, styleResult, skusResult ] = await Promise.all([photoQuery(), styleQuery(), skusQuery()]);
+    // var result =
+    return result;
+  } catch(err) {
+    console.error(err);
+  }
+}
+
 exports.getProducts = getProducts;
+exports.getSpecificProduct = getSpecificProduct;
+exports.getStyles = getStyles;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const { getProducts } = require('./api.js');
+const { getProducts, getSpecificProduct, getStyles } = require('./api.js');
 
 app.get('/', (req, res) => {
   res.send('GET request received');
@@ -11,16 +11,24 @@ app.get('/products', (req, res) => {
   var { page, count } = req.query;
   (async () => {
     var result = await getProducts(page, count);
+    // can body parser help with formatting of result here?
     res.status(200).send('GET Products result: ' + JSON.stringify(result.rows));
   })()
 })
 
 app.get('/products/:product_id', (req, res) => {
-  res.send('GET productid request received');
+  (async () => {
+    var result = await getSpecificProduct(req.params.product_id);
+    console.log('result: ', result);
+    res.status(200).send('GET productid result: ' + JSON.stringify(result));
+  })()
 })
 
 app.get('/products/:product_id/styles', (req, res) => {
-  res.send('GET styles request received');
+  (async () => {
+    var result = await getStyles(req.params.product_id);
+    res.status(200).send('GET styles result: ' + result);
+  })()
 })
 
 app.listen(port, () => {
