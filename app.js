cf7476ea4f1b167d213f852562202ee4bd1b86d9
mendelorian <ipanda303@gmail.com>
2022-02-18 12:39:51 -0800

    Added CORS headers to allow front end to work with my APIs

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const { routes } = require('./routes.js');
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, POST, GET, PUT, authorization"
+  );
+  next();
+})
 
 routes(app);
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { app } = require('./app.js');
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
