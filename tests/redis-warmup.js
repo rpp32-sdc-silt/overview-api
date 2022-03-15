f14383e004dd1cd993717ba1e8a4c8d342e5e006
mendelorian <ipanda303@gmail.com>
2022-03-14 21:55:49 -0700

    Cache warmup script via K6

---
+++ tests/redis-warmup.js
@@ -0,0 +1,16 @@
+const http = require('k6/http');
+const { sleep } = require('k6');
+export const options = {
+  vus: 1000,
+  duration: '5m'
+};
+
+
+export default function () {
+  // var lowerVal = 1000011 * 0.9;
+  // var productId = Math.floor(Math.random() * (1000011 - lowerVal) + lowerVal);
+  var productId = Math.floor(Math.random() * 1000011);
+
+  http.get(`http://localhost:8080/products/${productId}/styles`);
+  sleep(1);
+}
